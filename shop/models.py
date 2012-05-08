50a484c5c4794d87e4c194241fc275863cf25417
Jeremias Kangas <jeremias.kangas@gmail.com>
2012-05-08 22:55:07 +0200

    asd

--- shop/models.py
+++ shop/models.py
@@ -19,17 +19,16 @@ class Product(models.Model):
         return self.name
         
 class Purchase(models.Model):
-	created_at = models.DateTimeField(blank=True, default=datetime.datetime.now)
-	paid_at = models.DateTimeField(null=True, blank=True, default=None)
-	shipped_at = models.DateTimeField(null=True, blank=True, default=None)
+    created_at = models.DateTimeField(blank=True, default=datetime.datetime.now)
+    paid_at = models.DateTimeField(null=True, blank=True, default=None)
+    shipped_at = models.DateTimeField(null=True, blank=True, default=None)
 
     bitcoin_address = models.OneToOneField("BitcoinAddress")
 
-    # Not sure how this many-to-many thing will work.
     products = models.ManyToManyField("Product", through='ProductPurchase')
     address = models.TextField(blank=True)
 
 class ProductPurchase(models.Model):
-	product = models.ForeignKey(Product)
+    product = models.ForeignKey(Product)
     purchase = models.ForeignKey(Purchase)
-	count = models.IntegerField(blank=True, null=True)
+    count = models.IntegerField(blank=True, null=True)
