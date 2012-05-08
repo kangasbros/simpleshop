f939497a092bccc679a462fd02f633b59b4872c8
John Maguire <john@lulzplzkthx.com>
2012-05-08 16:16:28 -0400

    added BitcoinAddress, Product, and Purchase models. needs to be reviewed for i18n

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class BitcoinAddress(models.Model):
+    address = models.CharField(max_length=36)
+    
+    def __unicode__(self):
+        return self.address
+        
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    
+    def __unicode__(self)
+        return self.name
+        
+class Purchase(models.Model):
+    bitcoin_address = models.ForeignKey("BitcoinAddress")
+    # Not sure how this many-to-many thing will work.
+    products = models.ManyToManyField("Product")
+    address1 = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    # Not sure how to do international zip / state / province
+    postal_code = models.IntegerField()
+    state = models.CharField(max_length=2)
+    country = models.CharField(max_length=40)
+    
