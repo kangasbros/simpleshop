0cf739b43e6cc69387978fd3ae0192e7fff498e8
Jeremias Kangas <jeremias.kangas@gmail.com>
2012-05-08 22:27:45 +0200

    asd

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BitcoinAddress(models.Model):
+	"""A bitcoin address which can receive bitcoins"""
+	
+	created_at = models.DateTimeField(blank=True, default=datetime.datetime.now)
+	address = models.CharField(blank=True, max_length=100)
+	at_use = models.BooleanField(default=False)
+	
