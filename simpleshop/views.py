7b4a2dd6e83bcd1bc833d0c42b9270690b42c915
John Maguire <john@lulzplzkthx.com>
2012-05-08 21:12:30 -0400

    created form and templates, still need to add backend code

--- simpleshop/views.py
+++ simpleshop/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render_to_response
-from simpleshop.models import Product
+from simpleshop.models import *
+from simpleshop.forms import PurchaseForm
 
 def index(request):
-    product_list = Product.objects.all().order_by('price')
-    return render_to_response('index.html', {'product_list': product_list})
+    if request.method == 'POST':
+        form = PurchaseForm(request.POST)
+        if form.is_valid():
+            return render_to_response('payment.html', {
+                'shop_name': SHOP_NAME,
+                'shop_email': SHOP_FROM_EMAIL,
+                'bitcoin_address': '1Lauokt6UDNmHLC4RgoSS4Uc7sc6YYjQRn',
+                'cost': '20',
+                'email_address': 'example@example.com'
+            })
+        else:
+            return render_to_response('index.html', {
+                'form': form,
+                'error_message': 'Please fill out the form correctly and try again.'
+            })
+
+    form = PurchaseForm()
+    return render_to_response('index.html', {'form': form})
+    
