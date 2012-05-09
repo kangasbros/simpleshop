04fee9b7269847823cf906d8219777fa796fb79c
John Maguire <john@lulzplzkthx.com>
2012-05-09 16:01:40 -0400

    better error handling

--- simpleshop/views.py
+++ simpleshop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.core.mail import send_mail
 from simpleshop.models import *
 from simpleshop.forms import PurchaseForm
 from simpleshop import currency
@@ -10,21 +11,43 @@ def index(request):
         # If the form's valid, process the order.
         if form.is_valid():
             # Find an unused bitcoin address
-            bitcoinaddress = BitcoinAddress.objects.filter(used=False)[:1][0]
+            unused_bc_addresses = BitcoinAddress.objects.filter(used=False)[:100]
+            if not unused_bc_addresses:
+                return render_to_response('index.html', {
+                    'form': form,
+                    'error_message': 'We seem to have run out of Bitcoin addresses to process your order. Please try again in 10 minutes.',
+                })
+            if unused_bc_addresses.count() < 100:
+                send_mail('Almost out of Bitcoin addresses!',
+                    "There are less than 100 Bitcoin addresses left in the database.\n\nThought you'd wanna know!",
+                    SHOP_FROM_EMAIL, [SHOP_FROM_EMAIL],
+                    fail_silently=True)
+            
+            bitcoinaddress = unused_bc_addresses[0]
             
             # Save the information to the database
-            # Currently just using one product
-            purchase = Purchase.objects.create(
+            purchase = Purchase(
                 name=request.POST.get('name'),
                 email=request.POST.get('email'),
                 address=request.POST.get('address'),
                 bitcoin_address=bitcoinaddress)
-            product = Product.objects.all()[:1][0]
+                
+            # TODO: Add support for multiple products.
+            products = Product.objects.all()
+            if products.count() != 1:
+                return render_to_response('index.html', {
+                    'form': form,
+                    'error_message': 'An error has occured processing your request. Please try again later.',
+                })
+            product = products[0]
+            
+            purchase.save()
+            
+            # Connect the purchase to the products
             pp = ProductPurchase.objects.create(
                 product=product,
                 purchase=purchase,
                 count=request.POST.get('quantity'))
-            pp.save()
             
             # Finalize order (get price, mark address used)
             purchase.finalize_order()
