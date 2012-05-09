1dfe815e483ee6f0e5380f3a84536eb2f58f58d8
John Maguire <john@lulzplzkthx.com>
2012-05-09 13:29:20 -0400

    removed pointless view code, modifying format for model a bit, added variable confirmation support

--- simpleshop/views.py
+++ simpleshop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from simpleshop.models import *
 from simpleshop.forms import PurchaseForm
+from simpleshop import currency
 
 def index(request):
     if request.method == 'POST':
@@ -10,13 +11,13 @@ def index(request):
                 'shop_name': SHOP_NAME,
                 'shop_email': SHOP_FROM_EMAIL,
                 'bitcoin_address': '1Lauokt6UDNmHLC4RgoSS4Uc7sc6YYjQRn',
-                'cost': '20',
                 'email_address': 'example@example.com'
             })
         else:
             return render_to_response('index.html', {
                 'form': form,
-                'error_message': 'Please fill out the form correctly and try again.'
+                'cost': currency.currency2btc(100),
+                'error_message': 'Please fill out the form correctly and try again.',
             })
 
     form = PurchaseForm()
