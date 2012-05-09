1a59e64054fbc43d008a84bf21e191d361069a95
John Maguire <john@lulzplzkthx.com>
2012-05-09 13:31:05 -0400

    merging

--- simpleshop/models.py
+++ simpleshop/models.py
@@ -48,12 +48,12 @@ class BitcoinAddress(models.Model):
     used = models.BooleanField(default=False)
 
     def received(self):
-        url="http://blockchain.info/q/addressbalance/"+self.address+"?confirmations=2"
+        url = "http://blockchain.info/q/addressbalance/" + self.address + "?confirmations=" + BITCOIN_CONFIRMATIONS_REQUIRED
         f = urllib.urlopen(url, None)
         data = f.read()
-        r=Decimal(data)*Decimal("0.00000001")
-        if r>self.received_least:
-            self.received_least=r
+        r = Decimal(data) * Decimal("0.00000001")
+        if r > self.received_least:
+            self.received_least = r
             self.save()
         return r
     
@@ -73,42 +73,42 @@ class Purchase(models.Model):
     created_at = models.DateTimeField(blank=True, default=datetime.datetime.now)
     paid_at = models.DateTimeField(null=True, blank=True, default=None)
     shipped_at = models.DateTimeField(null=True, blank=True, default=None)
-
+    
     bitcoin_address = models.OneToOneField("BitcoinAddress")
     bitcoin_payment = models.DecimalField(null=True, max_digits=16, decimal_places=8, default=None)
     price_total = models.DecimalField(null=True, max_digits=8, decimal_places=2, default=None)
-
+    
     def check_payment_status(self):
         if not self.bitcoin_payment:
             raise Exception("total price not calculated")
         if self.paid_at:
             return True
-        if self.bitcoin_address.received()>self.bitcoin_payment:
-            self.paid_at=datetime.datetime.now()
-            list_products="\n\nProducts\n----\n"
+        if self.bitcoin_address.received() > self.bitcoin_payment:
+            self.paid_at = datetime.datetime.now()
+            list_products = "\n\nProducts\n----\n"
             for pp in ProductPurchase.objects.filter(purchase=self):
-                list_products+=pp.product.name+", "+pp.product.price+" "+BITCOIN_FIAT_CURRENCY+" X"+pp.count+"\n"
-            list_products+="\n----\n"
-            list_products+="Total: "+self.price_total+BITCOIN_FIAT_CURRENCY+"\n"
-            list_products+="Paid in bitcoins: "+self.bitcoin_payment+" BTC\n\n"
-            list_products+="Email: "+self.email+"\n"
-            list_products+="Shipping address:\n"+self.address+"\n"
-            send_mail(SHOP_CONFIRMATION_MESSAGE_SUBJECT, SHOP_CONFIRMATION_MESSAGE+list_products, SHOP_FROM_EMAIL, [self.email],\
+                list_products += pp.product.name+", " + pp.product.price+" " + BITCOIN_FIAT_CURRENCY + " X" + pp.count + "\n"
+            list_products += "\n----\n"
+            list_products += "Total: " + self.price_total+BITCOIN_FIAT_CURRENCY + "\n"
+            list_products += "Paid in bitcoins: " + self.bitcoin_payment + " BTC\n\n"
+            list_products += "Email: " + self.email + "\n"
+            list_products += "Shipping address:\n" + self.address + "\n"
+            send_mail(SHOP_CONFIRMATION_MESSAGE_SUBJECT, SHOP_CONFIRMATION_MESSAGE + list_products, SHOP_FROM_EMAIL, [self.email],\
                 fail_silently=False)
-            send_mail('New order:'+self.email, list_products, self.email, [SHOP_FROM_EMAIL],\
+            send_mail('New order received', list_products, self.email, [SHOP_FROM_EMAIL],\
                 fail_silently=False)
             self.save()
-
+    
     def finalize_order(self):
         if price_total:
             raise Exception("should do finalize_order only once")
-        total=Decimal(0)
+        total = Decimal(0)
         for pp in ProductPurchase.objects.filter(purchase=self):
-            total+=pp.count*pp.product.price
-        self.price_total=total
-        self.bitcoin_payment=btc2currency(self.price_total, BITCOIN_FIAT_CURRENCY)
+            total += pp.count * pp.product.price
+        self.price_total = total
+        self.bitcoin_payment = btc2currency(self.price_total, BITCOIN_FIAT_CURRENCY)
         self.save()
-
+    
     products = models.ManyToManyField("Product", through='ProductPurchase')
     address = models.TextField(blank=True)
     email = models.EmailField()
