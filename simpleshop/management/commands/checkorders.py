35904e739c34ac67c51789a73f29affd390eedc1
John Maguire <john@lulzplzkthx.com>
2012-05-09 21:52:48 -0400

    renamed and changed checkpayments to checkorders

--- simpleshop/management/commands/checkorders.py
+++ simpleshop/management/commands/checkorders.py
@@ -8,13 +8,13 @@ class Command(BaseCommand):
     
     def handle(self, *args, **options):
         # Execute command on all orders marked unpaid
-        orders = Purchase.objects.filter(paid_at=None)
+        orders = Order.objects.filter(paid_at=None)
         for order in orders:
             order.check_payment_status()
         
         # Execute on all unpaid orders older than 14 days
         # TODO: Set to variable prune time
-        orders = Purchase.objects.filter(paid_at=None, created_at__lte=(timezone.now()-datetime.timedelta(days=14)))
+        orders = Order.objects.filter(paid_at=None, created_at__lte=(timezone.now()-datetime.timedelta(days=14)))
         for order in orders:
             order.prune()
 
