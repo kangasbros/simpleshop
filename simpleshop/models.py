acffc84181110a009f0ccdbe41e115273bb798a0
John Maguire <john@lulzplzkthx.com>
2012-05-10 12:25:23 -0400

    rearranged some view and model logic, not finished

--- simpleshop/models.py
+++ simpleshop/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.core.mail import send_mail
+from django.core import exceptions
 from decimal import Decimal
 from currency import currency2btc
 
@@ -62,7 +63,11 @@ class BitcoinAddress(models.Model):
     address = models.CharField(max_length=36)
     received_least = models.DecimalField(max_digits=16, decimal_places=8, default=Decimal(0))
     used = models.BooleanField(default=False)
-
+    
+    # Return raw Bitcoin address
+    def __unicode__(self):
+        return self.address
+    
     def received(self):
         url = "http://blockchain.info/q/addressbalance/" + self.address + "?confirmations=" + str(BITCOIN_CONFIRMATIONS_REQUIRED)
         f = urllib.urlopen(url, None)
@@ -72,10 +77,7 @@ class BitcoinAddress(models.Model):
             self.received_least = r
             self.save()
         return r
-    
-    def __unicode__(self):
-        return self.address
-        
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     image_url = models.CharField(max_length=255)
@@ -83,9 +85,10 @@ class Product(models.Model):
     description = models.TextField()
     stock = models.PositiveIntegerField()
     
+    # Return product name
     def __unicode__(self):
         return self.name
-        
+
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     paid_at = models.DateTimeField(null=True, blank=True, default=None)
@@ -101,6 +104,45 @@ class Order(models.Model):
     email = models.EmailField()
     
     closed = models.BooleanField(default=False)
+    
+    # Return Order #<id>
+    def __unicode__(self):
+        return "Order #" + str(self.pk)
+    
+    # Run during model save call
+    def save(self, *args, **kwargs):
+        # Only run if currently creating the object
+        if not self.pk:
+            # Allocate an unused Bitcoin address
+            unused_bitcoin_addresses = BitcoinAddress.objects.filter(used=False)
+            if not unused_bitcoin_addresses:
+                raise ObjectDoesNotExist("Cannot allocate an unused Bitcoin address")
+            
+            if unused_bitcoin_addresses:
+                send_mail('Almost out of Bitcoin addresses!',
+                    "There are less than 100 Bitcoin addresses left in the database.\n\nThought'd you wanna know!",
+                    SHOP_FROM_EMAIL, [SHOP_FROM_EMAIL], fail_silently=True)
+            
+            self.bitcoin_address = unused_bitcoin_addresses[0]
+            
+            self.bitcoin_address.used = True
+            self.bitcoin_address.save()
+        
+        super(Order, self).save(*args, **kwargs)
+    
+    # Calculate total price in fiat currency and Bitcoin
+    def calculate_price(self):
+        # Loop through products in order and add to total price
+        total = 0
+        for op in OrderProduct.objects.filter(order=self):
+            total += op.count * op.product.price
+        self.total_price = total
+        
+        # Calculate Bitcoin price
+        self.bitcoin_payment = currency2btc(self.total_price, BITCOIN_FIAT_CURRENCY)
+        
+        # Save to database
+        self.save()
     
     # Admin functions
     def was_paid(self):
@@ -121,25 +163,6 @@ class Order(models.Model):
     was_shipped.boolean = True
     was_shipped.short_description = 'Shipped?'
     
-    # Allocate a Bitcoin address, calculate Bitcoin price
-    # TODO: Move Bitcoin address logic from view
-    def finalize_order(self):
-        if self.total_price:
-            raise Exception("should do finalize_order only once")
-        
-        total = Decimal(0)
-        for pp in OrderProduct.objects.filter(order=self):
-            total += pp.count * pp.product.price
-        
-        self.total_price = total
-        self.bitcoin_payment = currency2btc(self.total_price, BITCOIN_FIAT_CURRENCY)
-        self.save()
-        
-        self.bitcoin_address.used = True
-        self.bitcoin_address.save()
-        
-        return True
-    
     # Check whether payment has been received
     def check_payment_status(self):
         if not self.bitcoin_payment:
@@ -153,11 +176,11 @@ class Order(models.Model):
             self.save()
             
             list_products = "Products\n----------\n"
-            for pp in OrderProduct.objects.filter(order=self):
+            for op in OrderProduct.objects.filter(order=self):
                 list_products += pp.product.name + ", " + str(pp.product.price) + " " + BITCOIN_FIAT_CURRENCY + " x " + str(pp.count) + "\n"
                 
-                product = pp.product
-                product.stock -= pp.count
+                product = op.product
+                product.stock -= op.count
                 product.save()
             list_products += "----------\n"
             list_products += "Total: " + str(self.total_price) + BITCOIN_FIAT_CURRENCY + "\n"
@@ -179,8 +202,8 @@ class Order(models.Model):
         self.bitcoin_address.used = False
         self.bitcoin_address.save()
         
-        for pp in OrderProduct.objects.filter(order=self):
-            pp.delete()
+        for op in OrderProduct.objects.filter(order=self):
+            op.delete()
         
         send_mail(SHOP_PRUNED_ORDER_SUBJECT, SHOP_PRUNED_ORDER_MESSAGE, SHOP_FROM_EMAIL, [self.email],
             fail_silently=False)
@@ -188,9 +211,6 @@ class Order(models.Model):
         self.delete()
         
         return True
-    
-    def __unicode__(self):
-        return "Order #" + str(self.pk)
 
 class OrderProduct(models.Model):
     product = models.ForeignKey(Product)
--- simpleshop/views.py
+++ simpleshop/views.py
@@ -5,70 +5,50 @@ from simpleshop.forms import OrderForm
 from simpleshop import currency
 
 def index(request):
+    # Check whether any products are even in stock
     # TODO: Add support for multiple products.
     products = Product.objects.all()
     if products.count() != 1 or products[0].stock <= 0:
         return render_to_response('index.html', {
             'out_of_stock': True,
         })
-
+    
     # Check if the user has submitted the order form.
     if request.method == 'POST':
         form = OrderForm(request.POST)
         # If the form's valid, process the order.
         if form.is_valid():
-            # Find an unused bitcoin address
-            # TODO: Move logic to model
-            unused_bc_addresses = BitcoinAddress.objects.filter(used=False)[:100]
-            if not unused_bc_addresses:
-                return render_to_response('index.html', {
-                    'form': form,
-                    'error_message': 'We seem to have run out of Bitcoin addresses to process your order. Please try again in 10 minutes.',
-                })
-            if unused_bc_addresses.count() < 100:
-                send_mail('Almost out of Bitcoin addresses!',
-                    "There are less than 100 Bitcoin addresses left in the database.\n\nThought you'd wanna know!",
-                    SHOP_FROM_EMAIL, [SHOP_FROM_EMAIL],
-                    fail_silently=True)
-            
-            bitcoinaddress = unused_bc_addresses[0]
-            
             # Save the information to the database
-            order = Order(
+            order = Order.objects.create(
                 name=request.POST.get('name'),
                 email=request.POST.get('email'),
-                address=request.POST.get('address'),
-                bitcoin_address=bitcoinaddress)
-            
-            order.save()
+                address=request.POST.get('address'))
             
             # Connect the order to the products
             # TODO: Add support for multiple products.
             product = products[0]
-            pp = OrderProduct.objects.create(
+            op = OrderProduct.objects.create(
                 product=product,
                 order=order,
                 count=request.POST.get('quantity'))
             
-            # Finalize order (get price, mark address used)
-            order.finalize_order()
+            # Calculate price of the order
+            order.calculate_price()
             
             # Show them an adress to send money to.
             return render_to_response('payment.html', {
                 'shop_name': SHOP_NAME,
                 'shop_email': SHOP_FROM_EMAIL,
                 'price': order.bitcoin_payment,
-                'bitcoin_address': bitcoinaddress,
+                'bitcoin_address': order.bitcoin_address,
                 'email_address': request.POST.get('email'),
             })
         # The form isn't valid, so we return an error.
-        else:
-            return render_to_response('index.html', {
-                'form': form,
-                'error_message': 'Please fill out the form correctly and try again.',
-            })
+        return render_to_response('index.html', {
+            'form': form,
+            'form_error': True,
+        })
     
     # If we get to this point, the user hasn't submitted the form, so display it.
     form = OrderForm()
     return render_to_response('index.html', {'form': form})
-    
